8eb2b5e2448d024c77da7c789e26b13464d15718
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-07 19:46:09 +0300

    config:Добавил доддподдержку scss modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,10 +1,20 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      { loader: "css-loader", options: { modules: true } },
+      "sass-loader",
+    ],
+  };
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoaders];
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -18,7 +18,7 @@ export function buildWebpackConfig(
       clean: true,
     },
     module: {
-      rules: buildLoaders(),
+      rules: buildLoaders(options),
     },
     resolve: buildResolvers(),
     devtool: isDev ? "inline-source-map" : undefined,
