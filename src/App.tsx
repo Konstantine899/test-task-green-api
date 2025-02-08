e21b41a507876008c361804be108f64b2c8077f1
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-08 12:31:28 +0300

    feat:получаю настройки с green-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { LoginForm } from "./LoginForm/LoginForm";
 
 const App = () => {
-  return <div></div>;
+  const [idInstance, setIdInstance] = useState<string>(
+    localStorage.getItem("idInstance") || ""
+  );
+  const [apiTokenInstance, setApiTokenInstance] = useState<string>(
+    localStorage.getItem("apiTokenInstance") || ""
+  );
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
+    !!idInstance && !!apiTokenInstance
+  );
+
+  const handleLogin = (idInstance: string, apiTokenInstance: string) => {
+    setIdInstance(idInstance);
+    setApiTokenInstance(apiTokenInstance);
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div>
+      {<p>`idInstance:{idInstance}`</p>}
+      {<p>`apiTokenInstance:{apiTokenInstance}`</p>}
+      {<p>`isLoggedIn:{isLoggedIn}`</p>}
+      <LoginForm onLogin={handleLogin} />
+    </div>
+  );
 };
 
 export default App;
